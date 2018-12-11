55bba11139729deea28472c5f41a3932e773326c
Robert Schulte <schulte.robertw@outlook.com>
2018-12-11 09:22:29 -0600

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -4,9 +4,14 @@ export default class ColorSelector extends Component {
   
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      return <div key={str} onClick={this.handleClick} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
+
+  handleClick = (e) => {
+    const hexColorString = e._targetInst.key
+    this.props.matrixColorChanger(hexColorString)
+  }
   
   render() {
     return (
